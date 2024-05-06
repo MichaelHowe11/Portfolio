df56cec6e0af380cf54c7fcc934b134afb0bb6d3
MichaelHowe11 <michael.howell23@student.cart.org>
2024-05-05 20:07:18 -0700

    Update type.js

--- scripts/type.js
+++ scripts/type.js
@@ -5,6 +5,7 @@ document.addEventListener("DOMContentLoaded", function () {
     let origtext = document.getElementById("typer").innerHTML;
     document.getElementById("typer").innerHTML = ""
     let i = 0
+    let i2 = 32
     console.log(text2.substring(origtext.length + i - 1, origtext.length + i))
 
     function typing() {
@@ -19,25 +20,28 @@ document.addEventListener("DOMContentLoaded", function () {
         }
         i++
     }
+    let isorig = false;
     function correction() {
-        let isorig = false;
         if (document.getElementById("typer").innerHTML != origtext && !isorig) {
             let htmltext = document.getElementById("typer").innerHTML;
             htmltext = htmltext.slice(0, origtext.length + i - 1) + htmltext.slice(origtext.length + i);
             document.getElementById("typer").innerHTML = htmltext;
             console.log(htmltext);
             setTimeout(correction, 50);
-        } else if (isorig) {
-            i = 0
+        } else if (document.getElementById("typer").innerHTML === origtext) {
+            isorig = true
+        }
+        if (isorig === true) {
+            console.log("bruh")
             if (document.getElementById("typer").innerHTML != text2) {
-                document.getElementById("typer").innerHTML += text2.charAt(i)
+                document.getElementById("typer").innerHTML += text2.charAt(i2)
                 setTimeout(correction, 50)
             } else {
-                istyping = false
-                clearTimeout(typing)
+                clearTimeout(correction)
             }
         }
-        i--;
+        i2++
+        i--
         console.log("e");
     }
 
